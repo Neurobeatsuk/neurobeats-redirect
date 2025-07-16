dc672c4ea19c9a1b24adadcf8d61410e40ebf669
Neurobeatsuk <keely.thompson.uk@gmail.com>
2025-07-16 23:44:11 +0100

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,4 +1,11 @@
 export default function handler(req, res) {
-  const { code } = req.query;
-  res.redirect(`https://colab.research.google.com/?code=${code}`);
+  const { code, error } = req.query;
+
+  if (code) {
+    res.send(`Success! Copy this URL and paste it into Colab: ${req.url}`);
+  } else if (error) {
+    res.send(`Error: ${error}`);
+  } else {
+    res.send('No code or error found.');
+  }
 }
